caf967c65b565f1453b95dd4ba9f6353946e3207
borisPristupa <boris.pristupa@gmail.com>
2019-01-29 11:36:07 +0300

    SpringJavaMail branch

---
+++ SpringJavaMail/src/test/java/com/boris/simple/springjavamail/SpringjavamailApplicationTests.java
@@ -0,0 +1,17 @@
+package com.boris.simple.springjavamail;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringjavamailApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
